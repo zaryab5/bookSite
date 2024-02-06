f5c8242d38eda0c4cf0455a3ca27b01f413dd3c2
Zaryab <zaryab@gmail.com>
2024-02-06 17:56:02 +0500

    first

--- src/routers/router.jsx
+++ src/routers/router.jsx
@@ -27,7 +27,7 @@ import Users from "../components/Dashboard/Users";
 import UploadMany from "../components/Dashboard/UploadMany";
 
  
-  
+const api=import.meta.env.VITE_BASE_URL;
   const router = createBrowserRouter([
     {
       path: "/",
@@ -64,7 +64,7 @@ import UploadMany from "../components/Dashboard/UploadMany";
     {
       path:"/books/:id",
       element:<SingleBook/>,
-      loader:({params})=>fetch(`http://localhost:3000/books/${params.id}`)
+      loader:({params})=>fetch(`${api}/books/${params.id}`)
     }
   ]
     },
@@ -87,7 +87,7 @@ import UploadMany from "../components/Dashboard/UploadMany";
           {
             path:"/admin/dashboard/edit-books/:id",
             element:(<RequireAuth fallbackPath="/login"><EditBooks/></RequireAuth>),
-            loader:({params})=>fetch(`http://localhost:3000/books/${params.id}`)
+            loader:({params})=>fetch(`${api}/books/${params.id}`)
           },
           {
             path:"/admin/dashboard/user",
@@ -126,111 +126,3 @@ import UploadMany from "../components/Dashboard/UploadMany";
   export default router
 
 
-// import React from 'react';
-// import RequireAuth from '@auth-kit/react-router/RequireAuth';
-// import { Route, Routes } from 'react-router-dom';
-// import App from '../App';
-// import Blog from '../components/Blog';
-// import Home from '../components/Home';
-// import Shop from '../components/Shop';
-// import About from '../components/About';
-// import FilterSideBar from '../components/FilterSideBar';
-// import SingleBook from '../components/SingleBook';
-// import DashboardLayout from '../components/DashboardLayout';
-// import Dashboard from '../components/Dashboard/Dashboard';
-// import UploadBook from '../components/Dashboard/UploadBook';
-// import ManageBooks from '../components/ManageBooks';
-// import EditBooks from '../components/Dashboard/EditBooks';
-// import Login from '../components/Login';
-// import Table from '../components/Dashboard/Table';
-// import Contact from '../components/Contact';
-// import LibraryLayout from '../components/LibraryLayout';
-// import Register from '../components/Register';
-// import Main from '../components/Dashboard/Main';
-// import NotFound from '../components/NotFound';
-// import Comming from '../components/Dashboard/Comming';
-
-// const ProtectedRoute = ({ element, fallbackPath }) => (
-//   <RequireAuth fallbackPath={fallbackPath}>{element}</RequireAuth>
-// );
-
-// const routes = (
-//   <Routes>
-//     <Route path="/" element={<App />}>
-//       <Route index element={<Home />} />
-//       <Route path="/blog" element={<Blog />} />
-//       <Route path="/shop/:query" element={<Shop />} />
-//       <Route path="/shop/" element={<Shop />} />
-//       <Route path="/side" element={<FilterSideBar />} />
-//       <Route path="/contact" element={<Contact />} />
-//       <Route path="/about" element={<About />} />
-//       <Route
-//         path="/books/:id"
-//         element={
-//           <ProtectedRoute
-//             element={<SingleBook />}
-//             fallbackPath="/login"
-//           />
-//         }
-//         loader={({ params }) =>
-//           fetch(`http://localhost:3000/books/${params.id}`)
-//         }
-//       />
-//     </Route>
-//     <Route path="/admin/dashboard" element={<Dashboard />}>
-//       <Route
-//         index
-//         element={
-//           <ProtectedRoute
-//             element={<Main />}
-//             fallbackPath="/login"
-//           />
-//         }
-//       />
-//       <Route
-//         path="/admin/dashboard/upload"
-//         element={
-//           <ProtectedRoute
-//             element={<UploadBook />}
-//             fallbackPath="/login"
-//           />
-//         }
-//       />
-//       <Route
-//         path="/admin/dashboard/manage"
-//         element={
-//           <ProtectedRoute
-//             element={<Table />}
-//             fallbackPath="/login"
-//           />
-//         }
-//       />
-//       <Route
-//         path="/admin/dashboard/edit-books/:id"
-//         element={
-//           <ProtectedRoute
-//             element={<EditBooks />}
-//             fallbackPath="/login"
-//           />
-//         }
-//         loader={({ params }) =>
-//           fetch(`http://localhost:3000/books/${params.id}`)
-//         }
-//       />
-//       <Route
-//         path="/admin/dashboard/user"
-//         element={<Comming />}
-//       />
-//       <Route
-//         path="/admin/dashboard/comming-soon"
-//         element={<Comming />}
-//       />
-//     </Route>
-//     <Route path="/login" element={<Login />} />
-//     <Route path="/register" element={<Register />} />
-//     <Route path="/table" element={<ManageBooks />} />
-//     <Route path="/notFound" element={<NotFound />} />
-//   </Routes>
-// );
-
-// export default routes;
